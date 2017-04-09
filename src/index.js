586249df8e9cd859ac156d790a4c255db952b5a9
daphne remali <remalidaphne@gmail.com>
2017-04-10 01:49:42 +0200

    add index.js ReactDOM.render

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Base from './Base';
+import './index.css';
+
+ReactDOM.render(
+  <Base/>,
+  document.getElementById('root')
+);
